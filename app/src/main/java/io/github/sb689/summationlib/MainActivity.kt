ee0e6eda626e0ee3bd6b42d677c5c822d06eea69
sb689 <sujata689@gmail.com>
2021-09-08 13:37:51 -0500

    initial commit

---
+++ app/src/main/java/io/github/sb689/summationlib/MainActivity.kt
@@ -0,0 +1,17 @@
+package io.github.sb689.summationlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.github.sb689.sumlib.SumUp
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val result = SumUp().getSum(5,6)
+        Log.d("MainActivity", "sum result is  = $result")
+    }
+}
---
+++ sumLib/src/main/java/io/github/sb689/sumlib/SumUp.kt
@@ -0,0 +1,8 @@
+package io.github.sb689.sumlib
+
+class SumUp {
+
+ fun getSum(a: Int, b:Int):Int{
+     return a+b;
+ }
+}
