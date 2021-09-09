ff8c760cc06c8a182175953d0fd0bdec1d432c93
sb689 <sujata689@gmail.com>
2021-09-09 17:24:42 -0500

    testing

--- sumLib/src/main/java/io/github/sb689/sumlib/SumUp.kt
+++ sumLib/src/main/java/io/github/sb689/sumlib/SumUp.kt
@@ -3,7 +3,7 @@ package io.github.sb689.sumlib
 import android.content.Context
 import timber.log.Timber
 
-class SumUp constructor(var context: Context) {
+class SumUp  constructor(var context: Context) {
 
  fun getSum(a: Int, b:Int):Int{
      Timber.d("doing an addition")
