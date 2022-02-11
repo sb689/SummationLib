309d8d944b17f0a906cb77866198b95d303a49e0
sb689 <sujata689@gmail.com>
2022-02-10 18:14:41 -0600

    feature - testing app size

--- app/src/main/java/io/github/sb689/summationlib/MainActivity.kt
+++ app/src/main/java/io/github/sb689/summationlib/MainActivity.kt
@@ -2,6 +2,7 @@ package io.github.sb689.summationlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.sb689.sumlib.SumUp
 
 
 data class User(var userId: String, var name: String)
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val lib = SumUp().getSum(1,2)
+
 
     }
 
--- sumLib/src/main/java/io/github/sb689/sumlib/SumUp.kt
+++ sumLib/src/main/java/io/github/sb689/sumlib/SumUp.kt
@@ -4,6 +4,9 @@ import timber.log.Timber
 
 class SumUp {
 
+    fun tellAJoke():String{
+        return "it is summer here under 5 feet snpw, haha"
+    }
  fun getSum(a: Int, b:Int):Int{
      Timber.d("doing an addition")
      return a+b;
